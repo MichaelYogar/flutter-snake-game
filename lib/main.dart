56f5d45d02c98337941d798d1d4e3ade3063fd3a
Michael Yogar <michaelyogar878@gmail.com>
2022-01-08 17:00:12 -0500

    feat: snake grows and can move any direction

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -33,75 +32,176 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+enum Direction { up, down, left, right }
+
 class _MyHomePageState extends State<MyHomePage> {
   final Snake snake = Snake(positions: [11, 12, 13, 14]);
+ 
+  bool isStarted = false;
 
+  // Grid
+  final int gridSize = 170;
+  final rowSize = 10;
+
+  // Starting direction
+  Direction direction = Direction.right;
+
+  // Colours
+  Color backgroundColour = Colors.white;
+  Color gridColour = Color(0xff999c9a);
+  Color snakeColour = Colors.black;
+
+  // Snake grows every 500 milliseconds
   void begin() {
-    Timer.periodic(const Duration(milliseconds: 300), (Timer timer) {
+    Timer.periodic(const Duration(milliseconds: 500), (Timer timer) {
       setState(() {
-        snake.addTo(snake.positions.last + 1);
+        changeDirection();
+        if (!isStarted) {
+          timer.cancel();
+        }
       });
+    });
+  }
+
+  void end() {
+    setState(() {
+      isStarted = false;
+    });
+  }
+
+  void changeDirection() {
+    setState(() {
+      switch (direction) {
+        case Direction.up:
+          if (snake.positions.last < rowSize) {
+            snake.addTo(snake.positions.last - rowSize + gridSize);
+          } else {
+            snake.addTo(snake.positions.last - rowSize);
+          }
+          break;
+        case Direction.down:
+          if (snake.positions.last > gridSize - rowSize) {
+            snake.addTo(snake.positions.last + rowSize - gridSize);
+          } else {
+            snake.addTo(snake.positions.last + rowSize);
+          }
+          break;
+        case Direction.left:
+          if (snake.positions.last % rowSize == 0) {
+            snake.addTo(snake.positions.last - 1 + rowSize);
+          } else {
+            snake.addTo(snake.positions.last - 1);
+          }
+          break;
+        case Direction.right:
+          if ((snake.positions.last + 1) % rowSize == 0) {
+            snake.addTo(snake.positions.last + 1 - rowSize);
+          } else {
+            snake.addTo(snake.positions.last + 1);
+          }
+          break;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: GridView.builder(
-                    itemCount: 180,
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 10,
-                    ),
-                    itemBuilder: (BuildContext context, int index) {
-                      if (snake.positions.contains(index)) {
-                        return Container(
-                          padding: const EdgeInsets.all(1),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(7),
-                            child: Container(color: Colors.blue),
-                          ),
-                        );
-                      }
+      backgroundColor: backgroundColour,
+      body: SizedBox(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: GridView.builder(
+                  itemCount: gridSize,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: rowSize,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    if (snake.positions.contains(index)) {
                       return Container(
-                        padding: const EdgeInsets.all(1),
+                        padding: const EdgeInsets.all(0.5),
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(7),
-                          child: Container(color: Colors.black),
+                          child: Container(color: snakeColour),
                         ),
                       );
-                    }),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    bottom: 20.0, left: 20.0, right: 20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: () {
-                        begin();
-                      },
-                      child: Text(
-                        'Begin',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                        ),
+                    }
+                    return Container(
+                      padding: const EdgeInsets.all(0.5),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(1),
+                        child: Container(color: gridColour),
                       ),
-                    ),
-                  ],
+                    );
+                  }),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  color: Colors.black,
+                  iconSize: 15,
+                  onPressed: () {
+                    direction = Direction.left;
+                  },
                 ),
-              )
-            ],
-          ),
+                IconButton(
+                  icon: const Icon(Icons.arrow_forward),
+                  color: Colors.black,
+                  iconSize: 15,
+                  onPressed: () {
+                    direction = Direction.right;
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.arrow_upward),
+                  color: Colors.black,
+                  iconSize: 15,
+                  onPressed: () {
+                    direction = Direction.up;
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.arrow_downward),
+                  color: Colors.black,
+                  iconSize: 15,
+                  onPressed: () {
+                    direction = Direction.down;
+                  },
+                ),
+                GestureDetector(
+                  onTap: () {
+                    begin();
+                    setState(() {
+                      isStarted = true;
+                    });
+                  },
+                  child: const Text(
+                    'Begin',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    end();
+                  },
+                  child: const Text(
+                    'End',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
